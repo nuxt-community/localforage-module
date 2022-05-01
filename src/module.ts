88de81a4c5639a1a48c08ab203ba65aef3c83d4e
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2022-05-01 14:36:20 -0300

    fix: fix bridge support
    (cherry picked from commit d4165995660361f0787f7fbe063ad661bae80a50)

--- src/module.ts
+++ src/module.ts
@@ -1,4 +1,4 @@
-import { addAutoImport, addPlugin, createResolver, defineNuxtModule, isNuxt3 } from '@nuxt/kit'
+import { addAutoImport, addPlugin, createResolver, defineNuxtModule, isNuxt2 } from '@nuxt/kit'
 import { defu } from 'defu'
 import localforage from 'localforage'
 import { name, version } from '../package.json'
@@ -29,7 +29,7 @@ export default defineNuxtModule<ModuleOptions>({
   },
   setup (options, nuxt) {
     // Default runtimeConfig
-    const runtimeConfig = defu(nuxt.options.runtimeConfig.public.localForage, {
+    const runtimeConfig = defu(getRuntimeConfig(nuxt), {
       name: options.name,
       storeName: options.storeName,
       driver: options.driver,
@@ -46,7 +46,7 @@ export default defineNuxtModule<ModuleOptions>({
       }
     })
 
-    nuxt.options.runtimeConfig.public.localForage = runtimeConfig
+    setRuntimeConfig(nuxt, runtimeConfig)
 
     // Create resolver to resolve relative paths
     const { resolve } = createResolver(import.meta.url)
@@ -54,10 +54,24 @@ export default defineNuxtModule<ModuleOptions>({
 
     addPlugin(resolve(runtimeDir, 'plugin.client'))
 
-    if (isNuxt3()) {
-      addAutoImport([
-        { name: 'useLocalForage', as: 'useLocalForage', from: resolve(runtimeDir, 'composables') }
-      ].filter(Boolean))
-    }
+    addAutoImport([
+      { name: 'useLocalForage', as: 'useLocalForage', from: resolve(runtimeDir, 'composables') }
+    ].filter(Boolean))
   }
 })
+
+function getRuntimeConfig (nuxt: any) {
+  if (isNuxt2()) {
+    return nuxt.options.publicRuntimeConfig.localForage
+  } else {
+    return nuxt.options.runtimeConfig.public.localForage
+  }
+}
+
+function setRuntimeConfig (nuxt: any, config: ModuleOptions) {
+  if (isNuxt2()) {
+    nuxt.options.publicRuntimeConfig.localForage = config
+  } else {
+    nuxt.options.runtimeConfig.public.localForage = config
+  }
+}
